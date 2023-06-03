259933cb7d105f4071526589330250c942f34183
Xiaohu Guo <xiaohu.guo@stfc.ac.uk>
2023-06-04 00:12:01 +0100

    fix the typo

--- Pstream_test.C
+++ Pstream_test.C
@@ -1,21 +1,26 @@
-#include "fvCFD.H";
+#include "fvCFD.H"
 
 int main(int argc, char* argv[]){
+
+    #include "setRootCase.H"
+    #include "createTime.H"
+    #include "createMesh.H"
     argList::noCheckProcessorDirectories();
-    #include "setRootCase.H";
+    #include "setRootCase.H"
     if(!Pstream::parRun()){
         FatalErrorInFunction
         << ": This utility can only be run parallel"
         << exit(FatalError);
     }
-Pout<< "Hello from process " << Pstream::myProcNo() << endl;
 
-//calculate the totlal mesh volume
-scalar meshVolume(0.);
-forAll(mesh.V(), cellI){
-    meshVolume += mesh.V()[cellI];
-}
- 
+    Pout<< "Hello from process " << Pstream::myProcNo() << endl;
+
+    //calculate the totlal mesh volume
+    scalar meshVolume(0.);
+    forAll(mesh.V(), cellI){
+       meshVolume += mesh.V()[cellI];
+    }
+
 }
 
     
